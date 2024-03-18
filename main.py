87a043df14c3c0290355d38cbef61d623412ca9c
Assassin_Bro <157574787+AssassinBro@users.noreply.github.com>
2024-03-18 16:18:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from discord.ext import commands
 intents = discord.Intents.default() # Подключаем "Разрешения"
 intents.message_content = True
 # Задаём префикс и интенты
-bot = commands.Bot(command_prefix='!', intents=intents) 
+bot = commands.Bot(command_prefix='>', intents=intents) 
 
 # С помощью декоратора создаём первую команду
 @bot.command()
 async def ping(ctx):
-    await ctx.send('ты че ахуел сука меня пинговать?')
+    await ctx.send('meow')
 
 
 bot.run('MTIxODU2MTEzNzYzNzk4MjI4MA.GNSjeA.Vq94HE5vdqguomtIhsj0-PucfRovJD5wagMkVY')
