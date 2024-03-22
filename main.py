1220166581ec5f9b786f772c458dd3d3e4980879
Assassin_Bro <157574787+AssassinBro@users.noreply.github.com>
2024-03-22 19:01:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import discord # Подключаем библиотеку
 from discord.ext import commands, tasks
 import time
 import random2
+import configparser  # импортируем библиотеку
+from discord import client
+config = configparser.ConfigParser()  # создаём объекта парсера
+config.read("config.ini")  # читаем конфиг
+channel_id = int(config.get('Discord','channel'))
+token = config.get('Discord','token')
+owners = list(map(int,config.get('Discord','owners').split()))
 intents = discord.Intents.default() # Подключаем "Разрешения"
 intents.message_content = True
 async def cooldown():
     return random2.randint(0,30)
 # Задаём префикс и интенты
 bot = commands.Bot(command_prefix='>', intents=intents) 
-meow_list = ['мяу','мурь~','мряфк~','UwU','OwO','QwQ','meow~','мья!~','Мррррх~','Ньях','Ня','DwD','kill yourself now!']
+meow_list = ['мяу','мурь~','мряфк~','UwU','OwO','QwQ','meow~','мья!~','Мррррх~','Ньях','Ня','DwD','kill yourself now!','Мряфх!~']
 # С помощью декоратора создаём первую команду
 @bot.command()
 async def meow(ctx):
-    await ctx.send('meow')
+    await ctx.send(meow_list[random2.randint(0, len(meow_list)-1)])
 
 def is_owner(ctx):
-    return ctx.author.id in [683007560868954129, 788319552789413939]
+    return ctx.author.id in owners
+
 
 @bot.command()
 async def sex(ctx):
@@ -23,21 +31,17 @@ async def sex(ctx):
         await ctx.send('Ам..Нет пожалуй?_Немного покраснев,Юна повернула свой взгляд на дверь в ЛС... <@683007560868954129>_')
     else:
         await ctx.send('да пошев ти...')
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Мрявх!~')
 
+print("Бот запущен!")
 @tasks.loop(seconds=30)
 async def func():
-    channel = bot.get_channel(1189778371962474518)
+    channel = bot.get_channel(channel_id)
     if channel:
         await channel.send(meow_list[random2.randint(0, len(meow_list)-1)])
     else:
         print("Канал не найден или бот не имеет доступа к нему.")
-
 @bot.event
 async def on_ready():
     func.start()
-token = 'MTIxODU2MTEzNzYzNzk4MjI4MA.GNSjeA.Vq94HE5vdqguomtIhsj0-PucfRovJD5wagMkVY'
 bot.run(token)
 client.run(token)
