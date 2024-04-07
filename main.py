46f4caf59954ab7f38f581b4af79a3a3733b9928
Assassin_Bro <157574787+AssassinBro@users.noreply.github.com>
2024-04-07 21:24:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,12 @@ async def sex(inter):
         await inter.response.send_message('Ам..Нет пожалуй?_Немного покраснев,Юна повернула свой взгляд на дверь в ЛС... <@683007560868954129>_')
     else:
         await inter.response.send_message("да пошев ти...")
+@bot.slash_command(guild_ids=servers)
+async def socks(inter):
+    if is_owner(inter):
+        await inter.response.send_message('https://media.discordapp.net/attachments/975785515985010769/1183337251728527380/attachment.gif?ex=6624da32&is=66126532&hm=17b229b961c09fb524ff3b76330c60a46ef05a40efce4ab0a34357e8d506747d&')
+    else:
+        await inter.response.send_message("Аэээ...Держи ножки паши https://cdn.discordapp.com/attachments/1132291576039747677/1226594778708180993/IMG_1812.jpg?ex=66255668&is=6612e168&hm=a3e0766d6457f5bbb522a3a3e514ded30a71a410157988b043afffc4b60751c3&")
 
 print("Бот запущен!")
 @tasks.loop(seconds=30)
